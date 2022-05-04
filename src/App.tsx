f329ad7c6ee6702c753b4a43ba6d7ebd87f7447e
Freddy Robert Bedregal Ticona <freddy@Freddys-MacBook-Air.local>
2022-05-04 16:51:30 -0500

    Three sections created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="sidebar">
+        <h1>This is a sidebar</h1>
+      </div>
+
+      <div className="medium-side">
+        <h1>This is medium</h1>
+      </div>
+
+      <div className="aside">
+        <h1>This is aside</h1>
+      </div>
+
     </div>
   );
 }
