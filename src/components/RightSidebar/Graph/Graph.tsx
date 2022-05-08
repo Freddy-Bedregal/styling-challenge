e7862de4ea9ddb23094d5a8e4d032fd6ddcf100e
Freddy Robert Bedregal Ticona <freddy@Freddys-MacBook-Air.local>
2022-05-08 16:26:09 -0500

    Chart Component finished

--- src/components/RightSidebar/Graph/Graph.tsx
+++ src/components/RightSidebar/Graph/Graph.tsx
@@ -1,9 +1,43 @@
 import './Graph.css'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+    labels: [],
+    datasets: [
+      {
+        data: [200, 125, 75, 50],
+        backgroundColor: [
+          '#FF9F00',
+          '#689FF8',
+          '#4AC29D',
+          '#BCBECA'
+        ],
+        borderWidth: 1,
+        cutout: "90%",
+        borderRadius: 45,
+        rotation: 90,
+        spacing: 15,
+        borderColor: "transparent",
+      },
+    ],
+  };
+
+  const options = {
+      events: []
+  }
 
 export function Graph(){
     return (
         <div className="graph-container">
             <span className="graph-title">Storage</span>
+            
+            <div className='container-doughnut'>
+                 <Doughnut data={data} options={options}/>
+            </div>
+
             <div className="circlex">
                 <span className='circle-number'>85%</span>
                 <span className='circle-text-under'>Used</span>
