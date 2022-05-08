0b1c96fa6f729a4e00fb6887186c3b472e5162eb
Freddy Robert Bedregal Ticona <freddy@Freddys-MacBook-Air.local>
2022-05-07 19:54:08 -0500

    Large Card Hint modified

--- src/components/MediumPart/HintLargeCard/HintLargeCard.tsx
+++ src/components/MediumPart/HintLargeCard/HintLargeCard.tsx
@@ -3,9 +3,15 @@ import './HintLargeCard.css'
 export function HintLargeCard(){
     return (
         <div className='hint-large-card-container'>
-            <p className='name-text'>Name</p>
-            <p className='members-text'>Members</p>
-            <p className='last-modified-text'>Last Modified</p>
+            <div className='left-hint-large-card-container'>
+                <span className='name-text'>Name</span>
+            </div>
+
+            <div className='right-hint-large-card-container'>
+                <span className='members-text'>Members</span>
+                <span className='last-modified-text'>Last Modified</span>
+                <span></span>
+            </div>
         </div>
     )
 }
